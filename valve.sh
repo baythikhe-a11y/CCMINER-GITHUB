2488300a6c072cdf541646c8f56252210b8a9903
Master Bye <bye@example.com>
2026-04-02 01:50:11 +0700

    Set system to Auto 24/7

--- valve.sh
+++ valve.sh
@@ -1,5 +1,5 @@
 read -p "🌐 สั่ง GitHub จ่ายเน็ตกี่รอบดีครับ Master?: " n
 for ((i=1; i<=n; i++)); do
-  gh workflow run bye_signal.yml
+  gh workflow run bye_signal.yml --repo baythikhe-a11y/CCMINER-GITHUB
 done
 echo "✅ เรียบร้อย! เน็ตจากฟ้ากำลังวิ่งมาหาพี่แล้วครับ!"
